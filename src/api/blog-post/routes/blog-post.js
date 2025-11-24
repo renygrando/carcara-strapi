c9b9e9e5a8e9af68ffacb349012629d22592c569
Reny Adelino Grando <renygrando@MacBook-Air-de-Reny.local>
2025-11-24 12:33:44 -0300

    Configure public access for blog-post API

--- src/api/blog-post/routes/blog-post.js
+++ src/api/blog-post/routes/blog-post.js
@@ -6,4 +6,17 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::blog-post.blog-post');
+module.exports = createCoreRouter('api::blog-post.blog-post', {
+  config: {
+    find: {
+      auth: false,
+      policies: [],
+      middlewares: [],
+    },
+    findOne: {
+      auth: false,
+      policies: [],
+      middlewares: [],
+    },
+  },
+});
